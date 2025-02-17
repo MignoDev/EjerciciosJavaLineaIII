5c048b2047fcfcebf42a20aa158717b41c39c61f
mignodev <miguelbejaranoing@gmail.com>
2025-02-17 15:39:55 -0500

    comandos git

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,14 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        System.out.println("Repasando comandos git");
+        System.out.println("git status");
+        System.out.println("git add .");
+        System.out.println("git commit -m");
+        System.out.println("git push");
+        System.out.println("git branch");
+        System.out.println("git switch -c");
+        System.out.println("git switch");
     }
 }
